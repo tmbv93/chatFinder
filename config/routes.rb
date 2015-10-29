3f32b7bfb906c282f6eda777bf7383658050d1f2
tmbv93 <tmbv93@gmail.com>
2015-10-29 11:39:48 +0100

    forside

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'statics/home'
   resources :users
+
+  root 'statics#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
