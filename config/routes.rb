805241c2f9940cb964814d32a7be6e119b25cfe0
tmbv93 <tmbv93@gmail.com>
2015-11-03 15:52:02 +0100

    forside

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get 'login' => 'sessions#create', as: 'login'
   get 'logout' => 'sessions#destroy', as: 'logout'
+  get 'profile' => 'session#show', as: 'profile'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
